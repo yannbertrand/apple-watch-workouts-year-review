88279e1b42c58c60d6950a3ce149155bc00eaf67
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-10-18 21:28:47 +0200

    ✨ Add a workout statistics mapper method

--- test/mapper.test.js
+++ test/mapper.test.js
@@ -1,4 +1,4 @@
-const { mapWorkoutAttributes } = require('../lib/mapper')
+const { mapWorkoutAttributes, mapWorkoutStatisticsAttributes } = require('../lib/mapper')
 
 const sampleWorkout = require('./samples/apple-workout.json')
 
@@ -59,6 +59,71 @@ describe('mapWorkoutAttributes(attributes)', () => {
 
     it('should set a "endDate" ISO date key', () => {
       expect(result.endDate).toMatchSnapshot()
+    })
+  })
+})
+
+describe('mapWorkoutStatisticsAttributes(workout, attributes)', () => {
+  let workout
+  beforeEach(() => {
+    workout = mapWorkoutAttributes(sampleWorkout)
+  })
+
+  describe('with non handled type', () => {
+    it('should do nothing', () => {
+      const sampleAttributes = {
+        type: 'HKQuantityTypeIdentifierUnknown',
+        sum: 10,
+        unit: 'm/s',
+      }
+
+      expect(mapWorkoutStatisticsAttributes(workout, sampleAttributes)).toEqual(workout)
+    })
+  })
+
+  describe('with handled type', () => {
+    it('should handle the "energy" identifier', () => {
+      const sampleAttributes = {
+        type: 'HKQuantityTypeIdentifierActiveEnergyBurned',
+        sum: '32.742351690196',
+        unit: 'kcal',
+      }
+      const result = mapWorkoutStatisticsAttributes(workout, sampleAttributes)
+
+      expect(result.totalEnergyBurned).toEqual(32.742351690196)
+    })
+
+    it('should handle the "cycling distance" identifier', () => {
+      const sampleAttributes = {
+        type: 'HKQuantityTypeIdentifierDistanceCycling',
+        sum: '1.759895436402728',
+        unit: 'km',
+      }
+      const result = mapWorkoutStatisticsAttributes(workout, sampleAttributes)
+
+      expect(result.totalDistance).toEqual(1.759895436402728)
+    })
+
+    it('should handle the "swimming distance" identifier', () => {
+      const sampleAttributes = {
+        type: 'HKQuantityTypeIdentifierDistanceSwimming',
+        sum: '750',
+        unit: 'm',
+      }
+      const result = mapWorkoutStatisticsAttributes(workout, sampleAttributes)
+
+      expect(result.totalDistance).toEqual(0.75)
+    })
+
+    it('should handle the "walking/running distance" identifier', () => {
+      const sampleAttributes = {
+        type: 'HKQuantityTypeIdentifierDistanceWalkingRunning',
+        sum: '2101.3',
+        unit: 'm',
+      }
+      const result = mapWorkoutStatisticsAttributes(workout, sampleAttributes)
+
+      expect(result.totalDistance).toEqual(2.1013)
     })
   })
 })
